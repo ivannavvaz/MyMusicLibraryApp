c1a450ccc4091400b9c3acbcb14c4afcf5855c71
ivannavvaz <ivannavarrovazq@proton.me>
2024-02-06 21:18:57 +0100

    Creada activity auth

--- app/src/main/java/com/inavarro/mibibliotecamusical/authModule/AuthActivity.kt
+++ app/src/main/java/com/inavarro/mibibliotecamusical/authModule/AuthActivity.kt
@@ -2,8 +2,18 @@ package com.inavarro.mibibliotecamusical.authModule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.inavarro.mibibliotecamusical.R
+import com.inavarro.mibibliotecamusical.authModule.services.LoginService
+import com.inavarro.mibibliotecamusical.common.Constants
+import com.inavarro.mibibliotecamusical.common.entities.UserInfoEmail
+import com.inavarro.mibibliotecamusical.common.entities.UserInfoUsername
 import com.inavarro.mibibliotecamusical.databinding.ActivityAuthBinding
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class AuthActivity : AppCompatActivity() {
 
@@ -11,7 +21,81 @@ class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         mBinding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        mBinding.btnLogin.setOnClickListener {
+            login()
+        }
+
+        mBinding.btnRegister.setOnClickListener {
+            register()
+        }
+    }
+
+    private fun login() {
+        val user = mBinding.etUser.text.toString().trim()
+        val password = mBinding.etPassword.text.toString().trim()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(LoginService::class.java)
+
+        // Con Corrutinas
+        lifecycleScope.launch {
+            try {
+                if (user.contains("@")) {
+                    val result = service.loginUserByEmail(UserInfoEmail(user, password))
+                } else {
+                    val result = service.loginUserByUsername(UserInfoUsername(user, password))
+                }
+            } catch (e: Exception) {
+                (e as? HttpException)?.let {
+                    when(it.code()) {
+                        400 -> {
+                            Toast.makeText(this@AuthActivity, "Error 400", Toast.LENGTH_SHORT).show()
+                        }
+                        else ->
+                            Toast.makeText(this@AuthActivity, "Error", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun register() {
+        val user = mBinding.etUser.text.toString().trim()
+        val password = mBinding.etPassword.text.toString().trim()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(LoginService::class.java)
+
+        /*
+        lifecycleScope.launch {
+            try {
+                val result = service.registerUser(UserInfo(email, password))
+                updateUI("${Constants.TOKEN_PROPERTY}: ${result.token}")
+            } catch (e: Exception) {
+                (e as? HttpException)?.let {
+                    when(it.code()) {
+                        400 -> {
+                            updateUI(getString(R.string.main_error_server))
+                        }
+                        else ->
+                            updateUI(getString(R.string.main_error_response))
+                    }
+                }
+            }
+        }
+         */
+
     }
 }
