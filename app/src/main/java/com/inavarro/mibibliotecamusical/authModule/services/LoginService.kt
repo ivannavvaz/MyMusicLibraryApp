9b7a4aa2492728c5eb6c5d87d14ca059807e37a6
ivannavvaz <ivannavarrovazq@proton.me>
2024-02-09 15:52:37 +0100

    Cambiado return de los login service

--- app/src/main/java/com/inavarro/mibibliotecamusical/authModule/services/LoginService.kt
+++ app/src/main/java/com/inavarro/mibibliotecamusical/authModule/services/LoginService.kt
@@ -1,9 +1,10 @@
 package com.inavarro.mibibliotecamusical.authModule.services
 
 import com.inavarro.mibibliotecamusical.common.Constants
-import com.inavarro.mibibliotecamusical.common.entities.UserInfoEmail
-import com.inavarro.mibibliotecamusical.common.retrofit.login.LoginResponse
-import com.inavarro.mibibliotecamusical.common.entities.UserInfoUsername
+import com.inavarro.mibibliotecamusical.common.entities.User
+import com.inavarro.mibibliotecamusical.common.retrofit.dataclassRequest.user.UserInfoEmail
+import com.inavarro.mibibliotecamusical.common.retrofit.dataclassRequest.user.UserInfoUsername
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -11,10 +12,10 @@ import retrofit2.http.POST
 interface LoginService {
     @Headers("Content-Type: application/json")
     @POST(Constants.BASE_URL + Constants.LOGINBYUSENAME_PATH)
-    suspend fun loginUserByUsername(@Body data: UserInfoUsername): LoginResponse
+    suspend fun loginUserByUsername(@Body data: UserInfoUsername): Response<User>
 
     @Headers("Content-Type: application/json")
     @POST(Constants.BASE_URL + Constants.LOGINBYEMAIL_PATH)
-    suspend fun loginUserByEmail(@Body data: UserInfoEmail): LoginResponse
+    suspend fun loginUserByEmail(@Body data: UserInfoEmail): Response<User>
 
 }
