7ee7905d5d54f5b3a4bff77e745dad7d1a0499f3
ivannavvaz <ivannavarrovazq@proton.me>
2024-02-09 15:48:26 +0100

    Modificado MainActivity para la bottom app bar y sus fragments

--- app/src/main/java/com/inavarro/mibibliotecamusical/mainModule/MainActivity.kt
+++ app/src/main/java/com/inavarro/mibibliotecamusical/mainModule/MainActivity.kt
@@ -2,11 +2,72 @@ package com.inavarro.mibibliotecamusical.mainModule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.inavarro.mibibliotecamusical.R
+import com.inavarro.mibibliotecamusical.databinding.ActivityMainBinding
+import com.inavarro.mibibliotecamusical.mainModule.findFragment.FindFragment
+import com.inavarro.mibibliotecamusical.mainModule.homeFragment.HomeFragment
+import com.inavarro.mibibliotecamusical.mainModule.libraryFragment.LibraryFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mFragmentManager: FragmentManager
+    private lateinit var mActiveFragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        mFragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val findFragment = FindFragment()
+        val libraryFragment = LibraryFragment()
+
+        mActiveFragment = homeFragment
+
+        mFragmentManager.beginTransaction()
+            .add(
+                R.id.hostFragment, libraryFragment,
+                LibraryFragment::class.java.name)
+            .hide(libraryFragment).commit()
+        mFragmentManager.beginTransaction()
+            .add(
+                R.id.hostFragment, findFragment,
+                FindFragment::class.java.name)
+            .hide(findFragment).commit()
+        mFragmentManager.beginTransaction()
+            .add(
+                R.id.hostFragment, homeFragment,
+                HomeFragment::class.java.name).commit()
+
+        mBinding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.action_home -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(homeFragment).commit()
+                    mActiveFragment = homeFragment
+                    true
+                }
+                R.id.action_add -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(findFragment).commit()
+                    mActiveFragment = findFragment
+                    true
+                }
+                R.id.action_profile -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(libraryFragment).commit()
+                    mActiveFragment = libraryFragment
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
