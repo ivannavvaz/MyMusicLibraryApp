7fe27ddc124614074105d34907a5790cb3a174e4
ivannavvaz <ivannavarrovazq@proton.me>
2024-02-09 15:47:15 +0100

    Borrado ficheros LoginReponse y SuccesResponse

--- app/src/main/java/com/inavarro/mibibliotecamusical/common/retrofit/SuccessResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.inavarro.mibibliotecamusical.common.retrofit
-
-open class SuccessResponse(succes: Any)
--- app/src/main/java/com/inavarro/mibibliotecamusical/common/retrofit/login/LoginResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.inavarro.mibibliotecamusical.common.retrofit.login
-
-import com.inavarro.mibibliotecamusical.common.retrofit.SuccessResponse
-
-data class LoginResponse(var ok: String): SuccessResponse(ok)
