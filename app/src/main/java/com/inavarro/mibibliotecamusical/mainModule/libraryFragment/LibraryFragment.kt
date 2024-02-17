8438b302a98a75a25d53e579d190471cb25b7253
Maria Alves <mariaalvess473@gmail.com>
2024-02-17 19:13:07 +0100

    LibraryFragment acabado. Creados dos nuevos adapters, un nuevo service y modificados vista, controlador y drawables.

--- app/src/main/java/com/inavarro/mibibliotecamusical/mainModule/libraryFragment/LibraryFragment.kt
+++ app/src/main/java/com/inavarro/mibibliotecamusical/mainModule/libraryFragment/LibraryFragment.kt
@@ -1,16 +1,39 @@
 package com.inavarro.mibibliotecamusical.mainModule.libraryFragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.inavarro.mibibliotecamusical.R
+import com.inavarro.mibibliotecamusical.common.Constants
+import com.inavarro.mibibliotecamusical.common.entities.Playlist
 import com.inavarro.mibibliotecamusical.databinding.FragmentLibraryBinding
+import com.inavarro.mibibliotecamusical.mainModule.NewPlaylistFragment.NewPlaylistFragment
+import com.inavarro.mibibliotecamusical.mainModule.SongsFragment.SongsFragment
+import com.inavarro.mibibliotecamusical.mainModule.libraryFragment.adapters.GridFormatPlaylistListAdapter
+import com.inavarro.mibibliotecamusical.mainModule.libraryFragment.adapters.ListFormatPlaylistListAdapter
+import com.inavarro.mibibliotecamusical.mainModule.libraryFragment.adapters.OnClickListener
+import com.inavarro.mibibliotecamusical.mainModule.libraryFragment.service.LibraryService
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class LibraryFragment : Fragment() {
+class LibraryFragment : Fragment(), OnClickListener {
 
     private lateinit var mBinding: FragmentLibraryBinding
+
+    private lateinit var mListFormatPlaylistListAdapter: ListFormatPlaylistListAdapter
+    private lateinit var mGridFormatGridFormatPlaylistListAdapter: GridFormatPlaylistListAdapter
+
+    private lateinit var mLinearlayoutPlaylist: LinearLayoutManager
+    private lateinit var mGridlayoutPlaylist: GridLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +41,115 @@ class LibraryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         mBinding = FragmentLibraryBinding.inflate(inflater, container, false)
+
+        mBinding.cbFormat.setOnClickListener {
+
+            if (mBinding.rvGridFormat.visibility == GONE)
+                mBinding.rvGridFormat.visibility = VISIBLE
+            else
+                mBinding.rvGridFormat.visibility = GONE
+
+
+            if (mBinding.rvListFormat.visibility == GONE)
+                mBinding.rvListFormat.visibility = VISIBLE
+            else
+                mBinding.rvListFormat.visibility = GONE
+
+        }
+
+        mBinding.fabNewPlaylist.setOnClickListener {
+            launchNewPlaylistFragment()
+        }
+
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.rvGridFormat.visibility = GONE
+
+        setupRecyclerView()
+
+        getPlaylists()
+    }
+
+
+    private fun setupRecyclerView(){
+        mListFormatPlaylistListAdapter = ListFormatPlaylistListAdapter(this)
+
+        mLinearlayoutPlaylist = LinearLayoutManager(this.context)
+
+        mBinding.rvListFormat.apply {
+            setHasFixedSize(true)
+            layoutManager = mLinearlayoutPlaylist
+            adapter = mListFormatPlaylistListAdapter
+        }
+
+        mGridFormatGridFormatPlaylistListAdapter = GridFormatPlaylistListAdapter(this)
+
+        mGridlayoutPlaylist = GridLayoutManager(this.context, 2)
+
+        mBinding.rvGridFormat.apply {
+            setHasFixedSize(true)
+            layoutManager = mGridlayoutPlaylist
+            adapter = mGridFormatGridFormatPlaylistListAdapter
+        }
+    }
+
+    private fun getPlaylists(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(LibraryService::class.java)
+
+        lifecycleScope.launch {
+
+            try {
+                val result = service.getPlaylistUser(1) // Aquí debería ir el id del usuario
+
+                val playlists = result.body()!!
+
+                mListFormatPlaylistListAdapter.submitList(playlists)
+                mGridFormatGridFormatPlaylistListAdapter.submitList(playlists)
+
+            } catch (e: Exception) {
+                Log.e("SET PLAYLIST ERROR", e.message.toString())
+            }
+        }
+    }
+
+    override fun onClick(playlistEntity: Playlist) {
+        val fragment = SongsFragment()
+        val bundle = Bundle()
+        bundle.putLong("playlistId", playlistEntity.id)
+        fragment.arguments = bundle
+
+        val fragmentManager = getFragmentManager()
+        val fragmentTransaction = fragmentManager?.beginTransaction()
+        if (fragmentTransaction != null) {
+            fragmentTransaction.add(R.id.hostFragment, fragment)
+            fragmentTransaction.commit()
+
+            fragmentTransaction.addToBackStack(null)
+        }
+    }
+
+    private fun launchNewPlaylistFragment(args: Bundle? = null){
+        val fragment = NewPlaylistFragment()
+
+        if(args != null) fragment.arguments = args
+
+        val fragmentManager = getFragmentManager()
+        val fragmentTransaction = fragmentManager?.beginTransaction()
+        if (fragmentTransaction != null) {
+            fragmentTransaction.add(R.id.hostFragment, fragment)
+            fragmentTransaction.commit()
+
+            fragmentTransaction.addToBackStack(null)
+        }
+
     }
 }
---
+++ app/src/main/java/com/inavarro/mibibliotecamusical/mainModule/libraryFragment/adapters/GridFormatPlaylistListAdapter.kt
@@ -0,0 +1,96 @@
+package com.inavarro.mibibliotecamusical.mainModule.libraryFragment.adapters
+
+import android.content.Context
+import android.content.res.Resources
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.inavarro.mibibliotecamusical.R
+import com.inavarro.mibibliotecamusical.common.Constants
+import com.inavarro.mibibliotecamusical.common.entities.Playlist
+import com.inavarro.mibibliotecamusical.databinding.ItemPlaylistBinding
+import com.inavarro.mibibliotecamusical.mainModule.libraryFragment.LibraryFragment
+import kotlin.math.roundToInt
+
+class GridFormatPlaylistListAdapter(private val listener: LibraryFragment):
+    ListAdapter<Playlist, RecyclerView.ViewHolder>(PlaylistDiffCallBack()) {
+
+    private lateinit var context: Context
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ItemPlaylistBinding.bind(view)
+
+        fun setListener(playlistEntity: Playlist) {
+            with(binding.root) {
+                setOnClickListener { listener.onClick(playlistEntity) }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+
+        val view = LayoutInflater.from(context).inflate(R.layout.item_playlist, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val playlist = getItem(position)
+
+        if (position == 0 || isEven(position)) {
+            // Add padding 16dp to left items
+
+            //val parentWidth = (R.layout.fragment_library as View).width
+            //val rvWidth = ( View.).width
+            //val margin = (parentWidth - rvWidth) / 2
+
+            val layoutParams = holder.itemView.layoutParams as RecyclerView.LayoutParams
+            layoutParams.setMargins((16 * Resources.getSystem().displayMetrics.density).roundToInt(), 0, 0, 0)
+            holder.itemView.layoutParams = layoutParams
+        } else {
+            val layoutParams = holder.itemView.layoutParams as RecyclerView.LayoutParams
+            layoutParams.setMargins(0, 0, 0, 0)
+            holder.itemView.layoutParams = layoutParams
+        }
+
+        with(holder as ViewHolder) {
+            setListener(playlist)
+
+            Glide.with(context)
+                .load(Constants.DEFAULT_PLAYLIST_IMAGE)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .into(binding.ivPlaylist)
+
+            if (playlist.titulo.length > 11) {
+                val titulo = playlist.titulo.substring(0, 11) + "..."
+                binding.tvPlaylistName.text = titulo
+            } else {
+                binding.tvPlaylistName.text = playlist.titulo
+            }
+            binding.tvPlaylistUser.text = playlist.usuario.username
+
+        }
+    }
+
+    private fun isEven(pos: Int): Boolean {
+
+        return pos % 2 == 0
+    }
+
+    class PlaylistDiffCallBack: DiffUtil.ItemCallback<Playlist>() {
+        override fun areItemsTheSame(oldItem: Playlist, newItem: Playlist): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Playlist, newItem: Playlist): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
